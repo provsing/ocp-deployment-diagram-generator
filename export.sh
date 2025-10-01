a92e128ccf3e72dd4616bc8d299f71d807138c95
provsing <peter.rovsing.contractor@euronext.com>
2025-10-01 10:32:54 +0200

    updated export.sh to introduce svc objects in the export

--- export.sh
+++ export.sh
@@ -2,7 +2,26 @@
 
 # Fetch all namespaces with the 'ingresscontroller' label ending with '-dev'
 #namespaces_json=$(oc get namespace -o json)
-namespaces_json=$(oc get namespace -o json | jq '[.items[] | select(.metadata.labels.ingresscontroller or (.metadata.name | test("^pg-.*$")) or .metadata.name == "postgres")]')
+#namespaces_json=$(oc get namespace -o json | jq '[.items[] | select(.metadata.labels.ingresscontroller or (.metadata.name | test("^pg-.*$")) or .metadata.name == "postgres")]')
+
+routes_json=$(oc get route -A -o json)
+echo $routes_json > routes.json
+
+services_json=$(oc get services -A -o json | jq '[.items[] | { name: .metadata.name, namespace: .metadata.namespace, ports: .spec.ports, selector: .spec.selector }]')
+echo $services_json > svc.json
+
+namespaces_json=$(oc get namespace -o json | jq '[  .items[] | 
+    select( 
+            (
+                (.metadata.name | test("^es-.*-all-.*") ) or 
+                (.metadata.name | test("^pg-.*") ) 
+            )   and 
+                ( (.metadata.name | test("^pg-psql-.*") | not) and 
+                  (.metadata.name | test("^pg-grafana.*") | not ) and 
+                  (.metadata.name | test("^pg-messagehub-.*") | not ) and
+                  (.metadata.name | test("^pg-.*-phoenix-.*") | not )
+                ) 
+        )]')
 
 # Fetch all networkpolicies across all namespaces once
 networkpolicies_json=$(oc get networkpolicies --all-namespaces -o json)
@@ -15,6 +34,8 @@ result=$(jq -n \
   --argjson namespaces "$(echo "$namespaces_json" | jq '[.[] | { name: .metadata.name, labels: .metadata.labels }]')" \
   --argjson networkpolicies "$(echo "$networkpolicies_json" | jq '[.items[] | { name: .metadata.name, namespace: .metadata.namespace, ingress: .spec.ingress }]')" \
   --argjson deployments "$(echo "$deployments_json" | jq '[.items[] | { name: .metadata.name, namespace: .metadata.namespace }]')" \
+  --argjson routes "$(echo "$routes_json" | jq '[.items[] | { name: .metadata.name, namespace: .metadata.namespace, host: .spec.host, subdomain: .spec.subdomain, path: .spec.path, toService: .spec.to.name }]')" \
+  --slurpfile services svc.json \
   '
   {
     namespaces: (
@@ -28,6 +49,14 @@ result=$(jq -n \
               ),
               deployments: (
                 $deployments
+                | map(select(.namespace == $ns.name))
+              ),
+              routes: (
+                $routes
+                | map(select(.namespace == $ns.name))
+              ),
+              services: (
+                $services[]
                 | map(select(.namespace == $ns.name))
               )
             }
